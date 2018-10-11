2bd1e96ba48056a89dbb056d3c285eb0c15ec243
yacinemsd <messaoudi.y76@gmail.com>
2018-10-11 13:36:16 +0200

    style.display

---
+++ exercice2/2.js
@@ -0,0 +1,6 @@
+document.getElementsByTagName('a')[0].addEventListener('click', function (){
+ document.getElementById('text').style.display ='block';
+})
+document.getElementsByTagName('a')[1].addEventListener('click', function (){
+ document.getElementById('text').style.display ='none';
+})
