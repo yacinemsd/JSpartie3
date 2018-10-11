ec41211ad67eaf6065033fe1f09b6b85f0b0faf4
yacinemsd <messaoudi.y76@gmail.com>
2018-10-11 11:31:10 +0200

    style js

---
+++ exercice1/1.js
@@ -0,0 +1,9 @@
+document.getElementById('image1').addEventListener('mouseover', function (){
+image1.style.border = '3px solid black';
+
+})
+
+document.getElementById('image1').addEventListener('mouseout', function (){
+image1.style.border = '';
+
+})
