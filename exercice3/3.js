93b3c9268fc540306a650155898c9ac9fae71bfe
yacinemsd <messaoudi.y76@gmail.com>
2018-10-11 14:04:23 +0200

    style.color

---
+++ exercice3/3.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName('green')[0].addEventListener('click', function(){
+document.getElementById('text').style.color = 'green';
+})
+document.getElementsByClassName('red')[0].addEventListener('click', function(){
+document.getElementById('text').style.color = 'red';
+})
+document.getElementsByClassName('blue')[0].addEventListener('click', function(){
+document.getElementById('text').style.color = 'blue';
+})
